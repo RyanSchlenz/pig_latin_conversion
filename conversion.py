285935838b60c1421c040c5bd1fe0f67731f874b
Ryan Schlenz <96892298+RyanSchlenz@users.noreply.github.com>
2023-08-16 18:01:52 -0600

    Create conversion.py

---
+++ conversion.py
@@ -0,0 +1,35 @@
+#get sentence from user
+
+original = input("What sentence do you want to convert to pig latin?: ").strip().lower()
+
+
+# split sentence into words
+words = original.split()
+
+# loop through words and convert to pig latin
+new_words = []
+
+#if starts w/vowel, add "yay"
+for word in words:
+    if word[0] in "aeiou":
+        new_word = word + "yay"
+        new_words.append(new_word)
+        
+#otherwise, move the first consonant cluster to the end and add "ay"
+    else:
+        vowel_pos = 0
+        for letter in word:
+            if letter not in "aeiou":
+                vowel_pos = vowel_pos + 1
+            else:
+                break
+            cons = word[:vowel_pos]
+            the_rest = word[vowel_pos:]
+            new_word = the_rest + cons + "ay"
+            new_words.append(new_word)
+
+# stick words back together
+output = " ".join(new_words)
+
+# output the final string
+print(output)
